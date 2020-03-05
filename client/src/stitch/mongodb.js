580244f377ec6858bfe6d5f6eeae17c496722082
Alex Franzen <acfranzen@gmail.com>
2020-03-05 10:50:03 -0500

    new ODP stitch app linked

--- client/src/stitch/mongodb.js
+++ client/src/stitch/mongodb.js
@@ -1,6 +1,9 @@
 import { RemoteMongoClient } from 'mongodb-stitch-browser-sdk';
 import { app } from './app';
 
+// TEMP
+// const { AnonymousCredential } = require('mongodb-stitch-browser-sdk');
+
 // TODO: Initialize a MongoDB Service Client
 const mongoClient = app.getServiceClient(
   RemoteMongoClient.factory,
@@ -8,6 +11,30 @@ const mongoClient = app.getServiceClient(
 );
 
 // TODO: Instantiate a collection handle for todo.items
-const items = mongoClient.db('todo').collection('items');
+const items = mongoClient.db('oasis').collection('youth');
+
+// items.insertOne({ name: 'change++' });
+items.count().then(res => console.log(res));
+items.findOne().then(res => console.log(res));
+
+// app.auth
+//   .loginWithCredential(new AnonymousCredential())
+//   .then(user =>
+//     items.updateOne(
+//       { owner_id: app.auth.user.id },
+//       { $set: { number: 42 } },
+//       { upsert: true }
+//     )
+//   )
+//   .then(() =>
+//     items.find({ owner_id: app.auth.user.id }, { limit: 100 }).asArray()
+//   )
+//   .then(docs => {
+//     console.log('Found docs', docs);
+//     console.log('[MongoDB Stitch] Connected to Stitch');
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
 
 export { items };
