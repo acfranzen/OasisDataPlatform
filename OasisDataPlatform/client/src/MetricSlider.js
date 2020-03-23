d79abe458ecdcb78836268f9e476c86a9fecdd98
akatneni <57238903+akatneni@users.noreply.github.com>
2020-03-22 20:15:43 -0400

    Updated Slider

--- OasisDataPlatform/client/src/MetricSlider.js
+++ OasisDataPlatform/client/src/MetricSlider.js
@@ -2,11 +2,13 @@ import React, {Component} from "react";
 import "./MetricSlider.css";
 const checkImg = require("./check.png");
 
-//Takes 4 arguments: title, total, current, goal
+//Takes 5 arguments: title, total, current, goal, showPercents
 // title is name of metric
 // total is the overall number of students
 // current is the number currently reached
 // goal is the desired number of kids to reach
+// showPercents is false to show explicit values of current/goal/total.
+//          it is true to show current percentage/goal percentage.
 
 class MetricSlider extends Component {
     constructor(props) {
@@ -16,7 +18,8 @@ class MetricSlider extends Component {
             title: this.props.title,
             current: this.props.current,
             goal: this.props.goal,
-            total: this.props.total
+            total: this.props.total,
+            showPercents: this.props.showPercents
         }
     }
 
@@ -24,9 +27,15 @@ class MetricSlider extends Component {
         return(
             <div className="holder">
                 <div className="metric-text">{this.state.title}</div>
-                <div className="goal-text"> {(this.state.current/this.state.total*100).toFixed(0)}%/
-                    {(this.state.goal/this.state.total*100).toFixed(0)}%</div>
+                <GoalText
+                     current={this.state.current}
+                     goal={this.state.goal}
+                     total={this.state.total}
+                     showPercents={this.state.showPercents}/>
+
+                
                 <Bar percentage={this.state.current/this.state.goal*100}/>
+                <div className="percentage-text">{(this.state.current/this.state.goal*100).toFixed(0)}%</div>
                 <CheckBox percentage={this.state.current/this.state.goal*100}/>
             </div>
         )
@@ -51,6 +60,16 @@ const CheckBox = (props) => {
     }
     else {
         return <div className="no-check"/>
+    }
+}
+
+const GoalText = (props) => {
+    if(props.showPercents===true) {
+        return (<div className="goal-text"> {(props.current/props.total*100).toFixed(0)}%/
+        {(props.goal/props.total*100).toFixed(0)}%</div>)
+    }
+    else {
+        return (<div className="goal-text"> {props.current}/ {props.goal}/ {props.total}</div>)
     }
 }
 
